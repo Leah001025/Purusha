2ea30a37359e036b125d63ff87665cd9605c02ba
baekwinter <kksbiy0012@gmail.com>
2024-03-21 02:43:16 +0900

    [AddFix]_03_스폰매니저 수정
    01. 스폰 매니저 수정

--- Purusha/Assets/Scripts/KHR/SpawnManager.cs
+++ Purusha/Assets/Scripts/KHR/SpawnManager.cs
@@ -10,15 +10,22 @@ public class SpawnManager : SingleTon<SpawnManager>
 
     private void Awake()
     {
-
+        SpawnPoints = new List<Transform>();
     }
     public void SettingSpawnPoints()
     {
         GameObject spawnPointsObject = new GameObject("SpawnPoints");
-      
+
+        // Stage1-1_SpawnPoint 프리팹 리스트에 추가
         SpawnPoints.Add(ResourceManager.Instance.Instantiate("Monster/Stage1-1_SpawnPoint", spawnPointsObject.transform).transform);
+
+        // Stage1-2_SpawnPoint 프리팹 리스트에 추가
         SpawnPoints.Add(ResourceManager.Instance.Instantiate("Monster/Stage1-2_SpawnPoint", spawnPointsObject.transform).transform);
+
+
+        // Stage1-3_SpawnPoint 프리팹 리스트에 추가
         SpawnPoints.Add(ResourceManager.Instance.Instantiate("Monster/Stage1-3_SpawnPoint", spawnPointsObject.transform).transform);
-        
+
+
     }
 }
