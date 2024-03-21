d4eb312ae73372b96711fac86ddd4de4cf0746d0
baekwinter <kksbiy0012@gmail.com>
2024-03-22 02:03:42 +0900

    [Fix}_05_레이캐스트

---
+++ Purusha/Assets/Scripts/KHR/PlayerConeRaycastDetection.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerConeRaycastDetection : MonoBehaviour
+{
+    public float detectionInterval = 1.0f; //레이 캐스트 추가
+    public float detectionAngle = 45.0f; //레이 각도
+
+
+}
+
