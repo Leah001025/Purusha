99ab8c4820a21c46bf117ca7bd8de11ac7b2f1c2
baekwinter <kksbiy0012@gmail.com>
2024-03-22 10:46:23 +0900

    [Add]_05/플레이어레이캐스트스크립트 추가
    01. 플레이어레이캐스트 스크립트 추가

--- Purusha/Assets/Scripts/KHR/PlayerConeRaycastDetection.cs
+++ Purusha/Assets/Scripts/KHR/PlayerConeRaycastDetection.cs
@@ -1,13 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerConeRaycastDetection : MonoBehaviour
 {
-    public float detectionInterval = 1.0f; //레이 캐스트 추가
-    public float detectionAngle = 45.0f; //레이 각도
+    public float detectionInterval = 1.0f; // 레이캐스트를 수행할 주기
+    public float detectionAngle = 45.0f; // 레이의 각도
+    public float detectionDistance = 1.5f; // 레이의 거리
+    public string BattleScene__; // 전투 씬의 이름
+    public LayerMask monsterLayer; // 몬스터 레이어
+    private Vector3 startPos;
+    private float detectionTimer = 0.0f;
 
+    private void Start()
+    {
+        startPos = transform.position + (transform.up * 1f) + (transform.forward * 0.2f);
+    }
+    void Update()
+    {
+        detectionTimer += Time.deltaTime; // 경과 시간 누적
 
+        if (detectionTimer >= detectionInterval)
+        {
+            PerformConeRaycast();
+            detectionTimer = 0.0f; // 타이머 초기화
+        }
+    }
+
+    void PerformConeRaycast()
+    {
+        Vector3 forward = transform.forward;
+        RaycastHit[] hits = Physics.SphereCastAll(startPos, detectionDistance, forward, detectionAngle, monsterLayer);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            {
+                Debug.Log("바이오닉스를 감지했습니다. 바이오닉스 이름: " + hit.collider.name);
+                SceneManager.LoadScene("BattleScene__"); // 전투 씬으로 전환
+                return;
+            }
+        }
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Vector3 forward = transform.forward;
+    //    Quaternion spreadRotation = Quaternion.AngleAxis(-detectionAngle, Vector3.up);
+    //    Vector3 leftSpread = spreadRotation * forward;
+    //    spreadRotation = Quaternion.AngleAxis(detectionAngle, Vector3.up);
+    //    Vector3 rightSpread = spreadRotation * forward;
+
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawRay(startPos, forward * detectionDistance);
+    //    Gizmos.DrawRay(startPos, leftSpread * detectionDistance);
+    //    Gizmos.DrawRay(startPos, rightSpread * detectionDistance);
+    //}
 }
-
