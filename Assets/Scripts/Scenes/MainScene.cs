faaea6ce04d0d1e996c7aed2b0006c86f0cdb578
ParkTaeHo98 <154481176+ParkTaeHo98@users.noreply.github.com>
2024-05-01 11:50:51 +0900

    [Add]Sound
    1.사운드추가

--- Assets/Scripts/Scenes/MainScene.cs
+++ Assets/Scripts/Scenes/MainScene.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainScene : BaseScene
 {
     private void Start()
     {
-        SoundManager.Instance.SceneAudioStart("MainScene");
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName == "MainScene")
+        {
+            SoundManager.Instance.SceneAudioStart("MainScene");
+        }
         UIManager.Instance.charInventoryUI.Clear();
         UIManager.Instance.popups.Clear();
     }
